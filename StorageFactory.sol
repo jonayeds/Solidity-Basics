3047d0e70b2f92c615e59ff3a69b99372a071caf
jonayeds <jonayeeds@gmail.com>
2025-03-03 00:00:00 +0600

    completed interact with ABI

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,8 +4,28 @@ pragma solidity ^0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorage;
     function createSimpleStorageContract()public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorage = new SimpleStorage();
+        listOfSimpleStorage.push(newSimpleStorage);
     }
+
+    function sfStore (uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // (Address, ABI: Application Binary Interface ) --> to interact with another contract
+        SimpleStorage mySimpleStorage = listOfSimpleStorage[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorage[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
+
+
+
+// // Custom getter to return contract addresses instead of instances
+//     function getContractAddress(uint256 index) public view returns (address) {
+//         require(index < listOfSimpleStorage.length, "Index out of bounds");
+//         return address(listOfSimpleStorage[index]);
+//     }
 }
