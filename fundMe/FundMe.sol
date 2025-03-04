899b71e8949038bcddaa5942693da9b9a5da0b27
jonayeds <jonayeeds@gmail.com>
2025-03-04 14:30:29 +0600

    completed sending transection using payable function

--- fundMe/FundMe.sol
+++ fundMe/FundMe.sol
@@ -6,7 +6,11 @@ pragma solidity ^0.8.19;
 // Set A minimum funding value in USD
 
 contract FundMe{
-    function fund()public{
+    function fund()public payable { //--> payable: if a function allows payments from wallet
+        // Allow users to sent $
+        // Have a minimum $ sent
+        // require : Checker function if condition doesn`t fulfilled, transaction reverts
+        require(msg.value > 1e18, "didn't Sent enough..."); //--> 1e18 = 1*10^18
     }
 
     function withdraw()public{}
