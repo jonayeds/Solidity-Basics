9c7ca1fa41061375887fac81c74651120ccb7158
jonayeds <jonayeeds@gmail.com>
2025-03-04 14:16:15 +0600

    initiated fundme project

---
+++ fundMe/FundMe.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// ---- Features ----
+// withdraw Funds
+// Set A minimum funding value in USD
+
+contract FundMe{
+    function fund()public{
+    }
+
+    function withdraw()public{}
+}
